c568de1a48c36c4d7eb69d250c31fdcbdb41c5ca
chynnasevilleno <cnssevilleno@addu.edu.ph>
2018-12-05 14:44:42 +0800

    test commit

---
+++ app/src/main/java/com/example/cnssevilleno/activity2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.cnssevilleno.activity2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d("xxxboixxx", "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void exitApplication(View view) {
+        finish();
+        System.exit(0);
+    }
+}
