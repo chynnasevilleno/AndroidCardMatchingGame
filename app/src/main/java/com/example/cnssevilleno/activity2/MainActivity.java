80b39d4471247da9a5e81f6501361a6696ee938d
chynnasevilleno <cnssevilleno@addu.edu.ph>
2018-12-10 00:01:33 +0800

    Changed app image and icons, fullscreen

--- app/src/main/java/com/example/cnssevilleno/activity2/MainActivity.java
+++ app/src/main/java/com/example/cnssevilleno/activity2/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 
